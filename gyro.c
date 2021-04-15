2cfbba52c7bc58e4170678f0d47a5e2f135cbee4
vondesch <loic.vondeschwanden@epfl.ch>
2021-04-15 12:34:58 +0200

    création gyro

---
+++ gyro.c
@@ -0,0 +1,19 @@
+/*
+ * gyro.c
+ *
+ *  Created on: 15 avr. 2021
+ *      Author: Loic
+ */
+
+#include "ch.h"
+#include "hal.h"
+#include <imu.h>
+
+
+void get_position(void)
+{
+	imu_start();
+}
+
+
+
---
+++ gyro.h
@@ -0,0 +1,15 @@
+/*
+ * gyro.h
+ *
+ *  Created on: 15 avr. 2021
+ *      Author: Loic
+ */
+
+#ifndef GYRO_H_
+#define GYRO_H_
+
+void get_position(void);
+
+
+
+#endif /* GYRO_H_ */
