13d012d470e0ce7760a263a68476f599c373dcaf
vondesch <loic.vondeschwanden@epfl.ch>
2021-05-01 20:18:52 +0200

    intro regulator pour avancer

---
+++ pi_regulator.h
@@ -0,0 +1,15 @@
+/*
+ * pi_regulator.h
+ *
+ *  Created on: 1 mai 2021
+ *      Author: Loic
+ */
+
+#ifndef PI_REGULATOR_H_
+#define PI_REGULATOR_H_
+
+int16_t pi_regulator(float distance);
+
+
+
+#endif /* PI_REGULATOR_H_ */
